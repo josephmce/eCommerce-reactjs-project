54d2b8bf759548cfaa2269a13ec96997db2becf0
Joseph <joseph_mce@live.co.uk>
2026-03-23 16:01:39 +0000

    Small css fix

--- src/Components/ui/AppWrapper.jsx
+++ src/Components/ui/AppWrapper.jsx
@@ -2,12 +2,13 @@
 const AppWrapper = ({ children }) => {
   return (
 <div
-  className="min-h-screen text-white m-6 rounded-3xl"
+  className="text-white m-6 rounded-3xl"
   style={{
     background: `
       linear-gradient(229deg, rgba(0,0,0,0) 49.5%, rgba(52,65,72,1) 50%, rgba(41,43,45,1) 100%),
       linear-gradient(135deg, #344148 0%, #292B2D 100%)
-    `
+    `,
+    minHeight: `calc(100vh - 3.5rem)`,
   }}
 >
   {children}
